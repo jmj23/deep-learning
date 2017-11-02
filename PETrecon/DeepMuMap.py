8427435125382150380ba0bd0241a5e2f834162e
Jacob Johnson <jmjohnson33@wisc.edu>
2017-11-02 12:41:44 -0500

    Changed model to upsampling-based to avoid checkerboard artifacts

--- PETrecon/DeepMuMap.py
+++ PETrecon/DeepMuMap.py
@@ -8,14 +8,16 @@ import sys
 sys.path.insert(0,'/home/jmj136/KerasFiles')
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 from keras import optimizers
+from keras.models import load_model
 from matplotlib import pyplot as plt
 from my_callbacks import Histories
 import numpy as np
 import h5py
+import time
 
 #%%
 # Model Save Path/name
-model_filepath = 'MuMapModel.hdf5'
+model_filepath = 'MuMapModel_v{}.hdf5'.format(4)
 # Data path/name
 datapath = 'petrecondata_tvt.hdf5'
 
@@ -30,6 +32,7 @@ with h5py.File(datapath,'r') as f:
 #%% Model
 from keras.layers import Input, Cropping2D, Conv2D, concatenate
 from keras.layers import BatchNormalization, Conv2DTranspose, ZeroPadding2D
+from keras.layers import UpSampling2D
 from keras.layers.advanced_activations import ELU
 from keras.models import Model
 def BlockModel_reg(samp_input):
@@ -40,69 +43,86 @@ def BlockModel_reg(samp_input):
     
     # contracting block 1
     rr = 1
-    lay_conv1 = Conv2D(10*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(crop)
-    lay_conv3 = Conv2D(10*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(crop)
-    lay_conv51 = Conv2D(10*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(crop)
-    lay_conv52 = Conv2D(10*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
+    lay_conv1 = Conv2D(16*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(crop)
+    lay_conv3 = Conv2D(16*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(crop)
+    lay_conv51 = Conv2D(16*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(crop)
+    lay_conv52 = Conv2D(16*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
     lay_merge = concatenate([lay_conv1,lay_conv3,lay_conv52],name='merge_{}'.format(rr))
-    lay_conv_all = Conv2D(10*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
+    lay_conv_all = Conv2D(16*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
     bn = BatchNormalization()(lay_conv_all)
     lay_act = ELU(name='elu{}_1'.format(rr))(bn)
-    lay_stride = Conv2D(10*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
+    lay_stride = Conv2D(16*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
     lay_act = ELU(name='elu{}_2'.format(rr))(lay_stride)
     act_list = [lay_act]
     
     # contracting blocks 2-3
     for rr in range(2,4):
-        lay_conv1 = Conv2D(10*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(lay_act)
-        lay_conv3 = Conv2D(10*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(lay_act)
-        lay_conv51 = Conv2D(10*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(lay_act)
-        lay_conv52 = Conv2D(10*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
+        lay_conv1 = Conv2D(16*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(lay_act)
+        lay_conv3 = Conv2D(16*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(lay_act)
+        lay_conv51 = Conv2D(16*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(lay_act)
+        lay_conv52 = Conv2D(16*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
         lay_merge = concatenate([lay_conv1,lay_conv3,lay_conv52],name='merge_{}'.format(rr))
-        lay_conv_all = Conv2D(10*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
+        lay_conv_all = Conv2D(16*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
         bn = BatchNormalization()(lay_conv_all)
         lay_act = ELU(name='elu_{}'.format(rr))(bn)
-        lay_stride = Conv2D(10*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
+        lay_stride = Conv2D(16*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
         lay_act = ELU(name='elu{}_2'.format(rr))(lay_stride)
         act_list.append(lay_act)
     
     # expanding block 3
     dd=3
-    lay_deconv1 = Conv2D(10*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_act)
-    lay_deconv3 = Conv2D(10*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_act)
-    lay_deconv51 = Conv2D(10*dd, (3,3),padding='same',name='DeConv51_{}'.format(dd))(lay_act)
-    lay_deconv52 = Conv2D(10*dd, (3,3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
+    lay_deconv1 = Conv2D(16*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_act)
+    lay_deconv3 = Conv2D(16*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_act)
+    lay_deconv51 = Conv2D(16*dd, (3,3),padding='same',name='DeConv51_{}'.format(dd))(lay_act)
+    lay_deconv52 = Conv2D(16*dd, (3,3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
     lay_merge = concatenate([lay_deconv1,lay_deconv3,lay_deconv52],name='merge_d{}'.format(dd))
-    lay_deconv_all = Conv2D(10*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
+    lay_deconv_all = Conv2D(16*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
     bn = BatchNormalization()(lay_deconv_all)
     lay_act = ELU(name='elu_d{}'.format(dd))(bn)
-    lay_stride = Conv2DTranspose(10*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
-    lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
-        
+    
+    lay_up = UpSampling2D()(lay_act)
+#    lay_stride = Conv2DTranspose(16*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
+#    lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
+    
+    lay_cleanup = Conv2DTranspose(16*dd, (3, 3),name='cleanup{}_1'.format(dd))(lay_up)
+    lay_act = ELU(name='elu_cleanup{}_1'.format(dd))(lay_cleanup)
+    lay_cleanup = Conv2D(16*dd, (3,3), padding='same', name='cleanup{}_2'.format(dd))(lay_act)
+    bn = BatchNormalization()(lay_cleanup)
+    lay_act = ELU(name='elu_cleanup{}_2'.format(dd))(bn)
+    
     # expanding blocks 2-1
     expnums = list(range(1,3))
     expnums.reverse()
     for dd in expnums:
         lay_skip = concatenate([act_list[dd-1],lay_act],name='skip_connect_{}'.format(dd))
-        lay_deconv1 = Conv2D(10*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_skip)
-        lay_deconv3 = Conv2D(10*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_skip)
-        lay_deconv51 = Conv2D(10*dd, (3, 3),padding='same',name='DeConv51_{}'.format(dd))(lay_skip)
-        lay_deconv52 = Conv2D(10*dd, (3, 3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
+        lay_deconv1 = Conv2D(16*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_skip)
+        lay_deconv3 = Conv2D(16*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_skip)
+        lay_deconv51 = Conv2D(16*dd, (3, 3),padding='same',name='DeConv51_{}'.format(dd))(lay_skip)
+        lay_deconv52 = Conv2D(16*dd, (3, 3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
         lay_merge = concatenate([lay_deconv1,lay_deconv3,lay_deconv52],name='merge_d{}'.format(dd))
-        lay_deconv_all = Conv2D(10*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
+        lay_deconv_all = Conv2D(16*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
         bn = BatchNormalization()(lay_deconv_all)
         lay_act = ELU(name='elu_d{}'.format(dd))(bn)
-        lay_stride = Conv2DTranspose(10*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
-        lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
-    # classifier
-    lay_out = Conv2D(1,(1,1), activation='linear',name='output_layer')(lay_act)
+        
+        lay_up = UpSampling2D()(lay_act)
+#        lay_stride = Conv2DTranspose(16*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
+#        lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
+        
+        lay_cleanup = Conv2DTranspose(16*dd, (3, 3),name='cleanup{}_1'.format(dd))(lay_up)
+        lay_act = ELU(name='elu_cleanup{}_1'.format(dd))(lay_cleanup)
+        lay_cleanup = Conv2D(16*dd, (3,3), padding='same',name='cleanup{}_2'.format(dd))(lay_act)
+        bn = BatchNormalization()(lay_cleanup)
+        lay_act = ELU(name='elu_cleanup{}_2'.format(dd))(bn)
+        
+    lay_pad = ZeroPadding2D(padding=((0,2*padamt), (0,2*padamt)), data_format=None)(lay_act)
+        
+    # regressor
+    lay_out = Conv2D(1,(1,1), activation='linear',name='output_layer')(lay_pad)
     
-    zeropad = ZeroPadding2D(padding=((0,2*padamt), (0,2*padamt)), data_format=None)(lay_out)
-    
-    return Model(lay_input,zeropad)
+    return Model(lay_input,lay_out)
     
 #%% callbacks
-earlyStopping = EarlyStopping(monitor='val_loss',patience=6,verbose=1,mode='auto')
+earlyStopping = EarlyStopping(monitor='val_loss',patience=8,verbose=1,mode='auto')
 
 checkpoint = ModelCheckpoint(model_filepath, monitor='val_loss',verbose=0,
                              save_best_only=True, save_weights_only=False,
@@ -120,7 +140,7 @@ RegModel.compile(loss='MSE', optimizer=adopt)
 
 #%% training
 print('Starting training')
-numEp = 15
+numEp = 50
 b_s = 8
 history = RegModel.fit(x_train, y_train,
                    batch_size=b_s, epochs=numEp,
@@ -129,6 +149,9 @@ history = RegModel.fit(x_train, y_train,
                    callbacks=CBs)
 
 print('Training complete')
+
+print('Loading best model...')
+RegModel = load_model(model_filepath)
 
 score = RegModel.evaluate(x_test,y_test)
 print("")
@@ -153,7 +176,21 @@ plt.show()
 print('Generating samples')
 # regression result
 pr_bs = np.minimum(16,x_test.shape[0])
+time1 = time.time()
 output = RegModel.predict(x_test,batch_size=pr_bs)
+time2 = time.time()
+print('Infererence time: ',1000*(time2-time1)/x_test.shape[0],' ms per slice')
+
+from skimage.measure import compare_ssim as ssim
+SSIMs = [ssim(im1,im2) for im1, im2 in zip(y_test[...,0],output[...,0])]
+
+num_bins = 10
+fig3 = plt.figure()
+n, bins, patches = plt.hist(SSIMs, num_bins, facecolor='blue', edgecolor='black', alpha=0.5)
+plt.show()
+print('Mean SSIM of ', np.mean(SSIMs))
+print('SSIM range of ', np.round(np.min(SSIMs),3), ' - ', np.round(np.max(SSIMs),3))
+
 
 from VisTools import multi_slice_viewer0
-multi_slice_viewer0(np.c_[x_test[...,0],output[...,0],y_test[...,0]],[])
+multi_slice_viewer0(np.c_[x_test[...,0],output[...,0],y_test[...,0]],SSIMs)
