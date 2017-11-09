0550cced964eb98c248678f673a60c13b89060b7
Jacob Johnson <jmjohnson33@wisc.edu>
2017-11-09 12:44:55 -0600

    Made 3D data preparation script

--- PETrecon/PreparePETreconData3D_TVT.py
+++ PETrecon/PreparePETreconData3D_TVT.py
@@ -13,7 +13,7 @@ import os
 
 DataDir = 'MatData'
 filepath = "{}/PETreconTrainingData{:03d}.mat".format(DataDir, 1)
-datapath = 'petrecondata_tvt.hdf5'
+datapath = 'petrecondata3D_tvt.hdf5'
 numsubj = 11
 val_split = .2
 test_split = .2
@@ -31,7 +31,7 @@ for im in wims:
 for im in fims:
     im /= np.max(im)
 
-inputs = np.stack((wims,fims),axis=3)
+inputs = np.stack((wims,fims),axis=3)[np.newaxis,...]
 
 for file in globfiles[1:numsubj]:
     print(file[-7:-4])
@@ -45,7 +45,7 @@ for file in globfiles[1:numsubj]:
     for im in fims:
         im /= np.max(im)
     
-    new_inputs = np.stack((wims,fims),axis=3)
+    new_inputs = np.stack((wims,fims),axis=3)[np.newaxis,...]
     inputs = np.concatenate((inputs,new_inputs),axis=0)
     
 inputs[inputs<0] = 0
@@ -53,16 +53,16 @@ inputs[inputs<0] = 0
 #%% Targets
 print('Loading targets')
 mat = spio.loadmat(globfiles[0],squeeze_me=True)
-ims = mat['y']
-ims = np.rollaxis(ims.astype(np.float32, copy=False),2,0)
-targets = ims[...,np.newaxis]
+targs = mat['y']
+targs = np.rollaxis(targs.astype(np.float32, copy=False),2,0)
+targets = targs[np.newaxis,...,np.newaxis]
 
 for file in globfiles[1:numsubj]:
     print(file[-7:-4])
     mat = spio.loadmat(file,squeeze_me=True)
-    ims = mat['y']
-    ims = np.rollaxis(ims.astype(np.float32, copy=False),2,0)
-    new_targets = ims[...,np.newaxis]
+    targs = mat['y']
+    targs = np.rollaxis(targs.astype(np.float32, copy=False),2,0)
+    new_targets = targs[np.newaxis,...,np.newaxis]
     targets = np.concatenate((targets,new_targets),axis=0)
     
 #%% Split validation,testing data
@@ -104,21 +104,25 @@ except Exception as e:
 #%% augment training data
 
 # LR flips
-fl_inputs = np.flip(inputs,2)
-fl_targets = np.flip(targets,2)
+fl_inputs = np.flip(inputs,3)
+fl_targets = np.flip(targets,3)
+
+aug_inputs = np.concatenate((inputs,fl_inputs),axis=0)
+aug_targets = np.concatenate((targets,fl_targets),axis=0)
 
 # gamma corrections
-gammas = .5 + np.random.rand(inputs.shape[0])
-gm_inputs = np.copy(inputs)
-for ii in range(gm_inputs.shape[0]):
-    gm_inputs[ii,...,0] = skexp.adjust_gamma(gm_inputs[ii,...,0],gamma=gammas[ii])
-    gm_inputs[ii,...,1] = skexp.adjust_gamma(gm_inputs[ii,...,1],gamma=gammas[ii])
+for rr in range(3):
+    gammas = .25 + 1.5*np.random.rand(inputs.shape[0])
+    gm_inputs = np.copy(inputs)
+    for ii in range(gm_inputs.shape[0]):
+        gm_inputs[ii,...,0] = skexp.adjust_gamma(gm_inputs[ii,...,0],gamma=gammas[ii])
+        gm_inputs[ii,...,1] = skexp.adjust_gamma(gm_inputs[ii,...,1],gamma=gammas[ii])
+        
+    gm_targets = np.copy(targets)
     
-gm_targets = np.copy(targets)
-
-# combine together
-aug_inputs = np.concatenate((inputs,fl_inputs,gm_inputs),axis=0)
-aug_targets = np.concatenate((targets,fl_targets,gm_targets),axis=0)
+    # combine together
+    aug_inputs = np.concatenate((aug_inputs,gm_inputs),axis=0)
+    aug_targets = np.concatenate((aug_targets,gm_targets),axis=0)
 
 
 #%% finalize training data
@@ -139,16 +143,3 @@ with h5py.File(datapath, 'a') as hf:
     hf.create_dataset("train_targets",  data=aug_targets,dtype='f')
     
 print('done')
-
-del val_inputs
-del val_targets
-del test_inputs
-del test_targets
-del aug_inputs
-del aug_targets
-del inputs
-del fl_inputs
-del gm_inputs
-del targets
-del fl_targets
-del gm_targets
