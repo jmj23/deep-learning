6fe2c5c122fc040fff31a4a5a08a3ee3137731be
Jacob Johnson <jmjohnson33@wisc.edu>
2017-11-15 14:45:41 -0600

    Tweaks after testing. This version is now working

--- friendly/SegmentationTraining.py
+++ friendly/SegmentationTraining.py
@@ -27,7 +27,7 @@ os.environ["CUDA_VISIBLE_DEVICES"]="1"
 model_filepath = 'SegModel.hdf5'
 
 # Data path/name
-datapath = 'segmentation_data.hdf5'
+datapath = 'SegData.hdf5'
 # Data must be saved in a single HDF5 file in the following format:
 # two datasets: inputs "x" and targets "y"
 # inputs are of shape (samples,row,col,channels)
@@ -35,6 +35,24 @@ datapath = 'segmentation_data.hdf5'
 
 # choose whether to augment training data or not
 augment_data=False
+
+# amount of data to use as validation data
+valFrac = 0.20
+
+# maximum number of epochs (iterations) to train
+numEp = 20
+
+# model-specifying variables
+
+# sets the number of filters to use at each level in the model
+# increase if results are poor, decrease if model is too big
+filter_multiplier = 16
+
+# sets the number of Inception modules to use in the model
+# increase to get larger receptive field and deeper model
+# decrease if your images are too small
+numberOfBlocks = 3
+
 
 #%% Data Loading
 def LoadData(datapath):
@@ -47,13 +65,13 @@ def SplitData(x_data,y_data,val_split=.2):
     # get total number of samples
     numSamples = x_data.shape[0];
     # randomly select a portion of these samples as indices
-    val_inds=np.random.choice(np.arange(numSamples),np.round(val_split*numSamples),replace=False)
+    val_inds=np.random.choice(np.arange(numSamples),np.round(val_split*numSamples).astype(np.int),replace=False)
     # use those indices to take out validation data from training data
     val_x = np.take(x_data,val_inds,axis=0)
     val_y = np.take(y_data,val_inds,axis=0)
     # remove validation data from training data and randomize
-    train_x = np.random.permutation(np.delete(val_x,val_inds,axis=0))
-    train_y = np.random.permutation(np.delete(val_y,val_inds,axis=0))
+    train_x = np.random.permutation(np.delete(x_data,val_inds,axis=0))
+    train_y = np.random.permutation(np.delete(y_data,val_inds,axis=0))
     return train_x,train_y,val_x,val_y
 #%% data augmentation, if desired
 def generate_augmented_data(inputs,targets):
@@ -91,10 +109,10 @@ def BlockModel(input_shape,filt_num=16,numBlocks=3):
     lay_input = Input(shape=(input_shape[1:]),name='input_layer')
         
      #calculate appropriate cropping
-    mod = np.mod(input_shape[0:2],2**numBlocks)
+    mod = np.mod(input_shape[1:3],2**numBlocks)
     padamt = mod+2
     # calculate size reduction
-    startsize = np.max(input_shape[0:2]-padamt)
+    startsize = np.max(input_shape[1:3]-padamt)
     minsize = (startsize-np.sum(2**np.arange(1,numBlocks+1)))/2**numBlocks
     if minsize<4:
         raise ValueError('Too small of input for this many blocks. Use fewer blocks or larger input')
@@ -103,43 +121,43 @@ def BlockModel(input_shape,filt_num=16,numBlocks=3):
     
     # contracting block 1
     rr = 1
-    lay_conv1 = Conv2D(10*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(crop)
-    lay_conv3 = Conv2D(10*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(crop)
-    lay_conv51 = Conv2D(10*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(crop)
-    lay_conv52 = Conv2D(10*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
+    lay_conv1 = Conv2D(filt_num*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(crop)
+    lay_conv3 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(crop)
+    lay_conv51 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(crop)
+    lay_conv52 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
     lay_merge = concatenate([lay_conv1,lay_conv3,lay_conv52],name='merge_{}'.format(rr))
-    lay_conv_all = Conv2D(10*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
+    lay_conv_all = Conv2D(filt_num*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
     bn = BatchNormalization()(lay_conv_all)
     lay_act = ELU(name='elu{}_1'.format(rr))(bn)
-    lay_stride = Conv2D(10*rr,(3,3),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
+    lay_stride = Conv2D(filt_num*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
     lay_act = ELU(name='elu{}_2'.format(rr))(lay_stride)
     act_list = [lay_act]
     
     # contracting blocks 2-n 
     for rr in range(2,numBlocks+1):
-        lay_conv1 = Conv2D(10*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(lay_act)
-        lay_conv3 = Conv2D(10*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(lay_act)
-        lay_conv51 = Conv2D(10*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(lay_act)
-        lay_conv52 = Conv2D(10*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
+        lay_conv1 = Conv2D(filt_num*rr, (1, 1),padding='same',name='Conv1_{}'.format(rr))(lay_act)
+        lay_conv3 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv3_{}'.format(rr))(lay_act)
+        lay_conv51 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv51_{}'.format(rr))(lay_act)
+        lay_conv52 = Conv2D(filt_num*rr, (3, 3),padding='same',name='Conv52_{}'.format(rr))(lay_conv51)
         lay_merge = concatenate([lay_conv1,lay_conv3,lay_conv52],name='merge_{}'.format(rr))
-        lay_conv_all = Conv2D(10*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
+        lay_conv_all = Conv2D(filt_num*rr,(1,1),padding='valid',name='ConvAll_{}'.format(rr))(lay_merge)
         bn = BatchNormalization()(lay_conv_all)
         lay_act = ELU(name='elu_{}'.format(rr))(bn)
-        lay_stride = Conv2D(10*rr,(3,3),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
+        lay_stride = Conv2D(filt_num*rr,(4,4),padding='valid',strides=(2,2),name='ConvStride_{}'.format(rr))(lay_act)
         lay_act = ELU(name='elu{}_2'.format(rr))(lay_stride)
         act_list.append(lay_act)
-    
+        
     # expanding block n
     dd=numBlocks
-    lay_deconv1 = Conv2D(10*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_act)
-    lay_deconv3 = Conv2D(10*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_act)
-    lay_deconv51 = Conv2D(10*dd, (3,3),padding='same',name='DeConv51_{}'.format(dd))(lay_act)
-    lay_deconv52 = Conv2D(10*dd, (3,3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
+    lay_deconv1 = Conv2D(filt_num*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_act)
+    lay_deconv3 = Conv2D(filt_num*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_act)
+    lay_deconv51 = Conv2D(filt_num*dd, (3,3),padding='same',name='DeConv51_{}'.format(dd))(lay_act)
+    lay_deconv52 = Conv2D(filt_num*dd, (3,3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
     lay_merge = concatenate([lay_deconv1,lay_deconv3,lay_deconv52],name='merge_d{}'.format(dd))
-    lay_deconv_all = Conv2D(10*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
+    lay_deconv_all = Conv2D(filt_num*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
     bn = BatchNormalization()(lay_deconv_all)
     lay_act = ELU(name='elu_d{}'.format(dd))(bn)
-    lay_stride = Conv2DTranspose(10*dd,(3,3),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
+    lay_stride = Conv2DTranspose(filt_num*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
     lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
         
     # expanding blocks n-1
@@ -147,25 +165,25 @@ def BlockModel(input_shape,filt_num=16,numBlocks=3):
     expnums.reverse()
     for dd in expnums:
         lay_skip = concatenate([act_list[dd-1],lay_act],name='skip_connect_{}'.format(dd))
-        lay_deconv1 = Conv2D(10*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_skip)
-        lay_deconv3 = Conv2D(10*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_skip)
-        lay_deconv51 = Conv2D(10*dd, (3, 3),padding='same',name='DeConv51_{}'.format(dd))(lay_skip)
-        lay_deconv52 = Conv2D(10*dd, (3, 3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
+        lay_deconv1 = Conv2D(filt_num*dd,(1,1),padding='same',name='DeConv1_{}'.format(dd))(lay_skip)
+        lay_deconv3 = Conv2D(filt_num*dd,(3,3),padding='same',name='DeConv3_{}'.format(dd))(lay_skip)
+        lay_deconv51 = Conv2D(filt_num*dd, (3, 3),padding='same',name='DeConv51_{}'.format(dd))(lay_skip)
+        lay_deconv52 = Conv2D(filt_num*dd, (3, 3),padding='same',name='DeConv52_{}'.format(dd))(lay_deconv51)
         lay_merge = concatenate([lay_deconv1,lay_deconv3,lay_deconv52],name='merge_d{}'.format(dd))
-        lay_deconv_all = Conv2D(10*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
+        lay_deconv_all = Conv2D(filt_num*dd,(1,1),padding='valid',name='DeConvAll_{}'.format(dd))(lay_merge)
         bn = BatchNormalization()(lay_deconv_all)
         lay_act = ELU(name='elu_d{}'.format(dd))(bn)
-        lay_stride = Conv2DTranspose(10*dd,(3,3),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
+        lay_stride = Conv2DTranspose(filt_num*dd,(4,4),strides=(2,2),name='DeConvStride_{}'.format(dd))(lay_act)
         lay_act = ELU(name='elu_d{}_2'.format(dd))(lay_stride)
+                
+    lay_pad = ZeroPadding2D(padding=((0,padamt[0]), (0,padamt[1])), data_format=None)(lay_act)
     # classifier
-    lay_out = Conv2D(1,(1,1), activation='sigmoid',name='output_layer')(lay_act)
+    lay_out = Conv2D(1,(1,1), activation='sigmoid',name='output_layer')(lay_pad)
     
-    lay_pad = ZeroPadding2D(padding=((0,padamt[0]), (0,padamt[1])), data_format=None)(lay_out)
-    
-    return Model(lay_input,lay_pad)
+    return Model(lay_input,lay_out)
 #%% Generate and compile model
-def GetModel(x_train):
-    Model = BlockModel(x_train.shape)
+def GetModel(x_train,numFilts=16,numBlocks=3):
+    Model = BlockModel(x_train.shape,numFilts,numBlocks)
     adopt = optimizers.adam()
     Model.compile(loss=dice_coef_loss, optimizer=adopt,
                   metrics=[dice_loss])
@@ -186,7 +204,7 @@ if __name__ == "__main__":
     print("Data loaded.")
     
     print("Separating data into validation and test...")
-    x_train,y_train,x_val,y_val=SplitData(x_data,y_data)
+    x_train,y_train,x_val,y_val=SplitData(x_data,y_data,val_split=valFrac)
     
     if augment_data:
         print("Augmenting data...")
@@ -196,11 +214,10 @@ if __name__ == "__main__":
     CBs = SetCallbacks()
     
     print("Building model...")
-    SegModel = GetModel(x_train)
+    SegModel = GetModel(x_train,filter_multiplier,numberOfBlocks)
     
     print('Starting training')
-    numEp = 20
-    b_s = np.minimum(np.maximum(np.round(x_train.shape[0]/20),4),16)
+    b_s = np.minimum(np.maximum(np.round(x_train.shape[0]/20),4),16).astype(np.int)
     history = SegModel.fit(x_train, y_train,
                        batch_size=b_s, epochs=numEp,
                        validation_data=(x_val,y_val),
