118aa370aa8011086a0dd807048bd5316a76c574
Jacob Johnson <johnsonj118@gmail.com>
2017-12-19 12:08:26 -0600

    Fixed zero-mask cleanup so that those slices are also deleted from the slice counts

--- DataSelectionPrep.py
+++ DataSelectionPrep.py
@@ -11,7 +11,7 @@ import scipy.io as spio
 import skimage.exposure as skexp
 import os
 
-DataDir = '/home/jmj/KerasFiles/MatData'
+DataDir = 'MatData'
 numsubj = 120
 numTest = 20
 np.random.seed(seed=1)
@@ -59,10 +59,10 @@ for file in globfiles[1:numsubj]:
     fatims = fatims[botind:topind]
     Vcut = Vcut[botind:topind]
     numSlice = topind-botind
-    sliceVec.append([numSlice])
+    sliceVec.append(numSlice)
     new_inputs = np.stack((waterims,fatims,Vcut),axis=-1)
     inputs = np.concatenate((inputs,new_inputs),axis=0)
-sliceVec = np.array(sliceVec)
+    
 #%% Targets
 print('Loading targets')
 mat = spio.loadmat(globfiles[0],squeeze_me=True)
@@ -84,95 +84,47 @@ for file in globfiles[1:numsubj]:
     new_targets = SegMask[botind:topind].reshape(numS,256,256,1)
     targets = np.concatenate((targets,new_targets),axis=0)
     
-#%% Calculate ranking and split validation,testing data
-
 #eliminate zero masks in targets
 targ_sums = np.sum(targets[...,0],axis=(1,2))
 zeroinds = np.argwhere(targ_sums==0)
 inputs = np.delete(inputs,zeroinds,axis=0)
 targets = np.delete(targets,zeroinds,axis=0)
+# update slice counts
+sliceVec = np.array(sliceVec)
+sliceCumArray = np.concatenate((np.array([0]),np.cumsum(sliceVec)),axis=0)
+for zz in range(zeroinds.shape[0]):
+    zind = zeroinds[zz]
+    subj = np.argwhere(sliceCumArray>zind)[0]-1
+    sliceVec[subj] = sliceVec[subj]-1
+    
 
-with np.errstate(divide='ignore', invalid='ignore'):
-    PDWF = np.nan_to_num(np.divide(inputs[...,0],(inputs[...,0]+inputs[...,1])))
-PDWFvals = np.sum(targets[...,0]*PDWF,axis=(1,2))/np.sum(targets[...,0],axis=(1,2))
-sortorder = np.argsort(PDWFvals)
-
-valrange = np.round(np.arange(0,len(sortorder),1/val_split)).astype(np.int)
-val_inds = sortorder[valrange]
-
-val_inputs = np.take(inputs,val_inds,axis=0)
-val_targets = np.take(targets,val_inds,axis=0)
-
-inputs = np.delete(inputs,val_inds,axis=0)
-targets = np.delete(targets,val_inds,axis=0)
-PDWFvals = np.delete(PDWFvals,val_inds)
-sortorder = np.argsort(PDWFvals)
-
-testrange = np.round(np.arange(0,len(sortorder),1/test_split)).astype(np.int)
-test_inds = sortorder[testrange]
-
+#%% Split training, testing data
+# get slice ranges corresponding to subjects
+sliceCumArray = np.concatenate((np.array([0]),np.cumsum(sliceVec)),axis=0)
+# pick out subjects
+numSubjs = sliceVec.shape[0]
+test_subjs = np.random.choice(np.arange(numSubjs),numTest,replace=False)
+# Get testing data
+test_inds = np.concatenate(([np.arange(sliceCumArray[ind],sliceCumArray[ind+1]) for ind in test_subjs]))
 test_inputs = np.take(inputs,test_inds,axis=0)
 test_targets = np.take(targets,test_inds,axis=0)
-
+# Remove from training arrays
 inputs = np.delete(inputs,test_inds,axis=0)
 targets = np.delete(targets,test_inds,axis=0)
 
-# store validation and testing data to HDF5 file
+# update slice ranges
+sliceVec = np.delete(sliceVec,test_subjs)
+sliceCumArray = np.concatenate((np.array([0]),np.cumsum(sliceVec)),axis=0)
+
+# store training and testing data to HDF5 file
 print('storing validation data as HDF5')
-with h5py.File('segdata_tvt.hdf5', 'x') as hf:
-    hf.create_dataset("val_inputs",  data=val_inputs,dtype='f')
+with h5py.File('DataSelectionData.hdf5', 'x') as hf:
+    hf.create_dataset("train_inputs",  data=inputs,dtype='f')
     hf.create_dataset("test_inputs", data=test_inputs,dtype='f')
-with h5py.File('segdata_tvt.hdf5', 'a') as hf:
-    hf.create_dataset("val_targets",  data=val_targets,dtype='i')
+    hf.create_dataset("train_targets",  data=targets,dtype='i')
     hf.create_dataset("test_targets",  data=test_targets,dtype='i')
-del val_inputs
-del val_targets
+    hf.create_dataset("CumSliceArray",data=sliceCumArray,dtype='i')
 del test_inputs
 del test_targets
-
-#%% augment training data
-
-# LR flips
-fl_inputs = np.flip(inputs,2)
-fl_targets = np.flip(targets,2)
-
-# gamma corrections
-gammas = .5 + np.random.rand(inputs.shape[0])
-gm_inputs = np.copy(inputs)
-for ii in range(gm_inputs.shape[0]):
-    gm_inputs[ii,...,0] = skexp.adjust_gamma(gm_inputs[ii,...,0],gamma=gammas[ii])
-    gm_inputs[ii,...,1] = skexp.adjust_gamma(gm_inputs[ii,...,1],gamma=gammas[ii])
-    
-gm_targets = np.copy(targets)
-
-# combine together
-aug_inputs = np.concatenate((inputs,fl_inputs,gm_inputs),axis=0)
-aug_targets = np.concatenate((targets,fl_targets,gm_targets),axis=0)
 del inputs
-del fl_inputs
-del gm_inputs
 del targets
-del fl_targets
-del gm_targets
-
-#%% finalize training data
-
-# randomize inputs
-print('randomizing training inputs')
-numS = aug_inputs.shape[0]
-sort_r = np.random.permutation(numS)
-np.take(aug_inputs,sort_r,axis=0,out=aug_inputs)
-np.take(aug_targets,sort_r,axis=0,out=aug_targets)
-
-
-# store training data
-print('storing train data as HDF5')
-with h5py.File('segdata_tvt.hdf5', 'a') as hf:
-    hf.create_dataset("train_inputs",  data=aug_inputs,dtype='f')
-with h5py.File('segdata_tvt.hdf5', 'a') as hf:
-    hf.create_dataset("train_targets",  data=aug_targets,dtype='i')
-    
-print('done')
-
-del aug_inputs
-del aug_targets
