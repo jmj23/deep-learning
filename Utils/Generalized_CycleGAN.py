34ee14c4203f3b5f5fa961e7723addc9ae8a0e3b
Jacob Johnson <jmjohnson33@wisc.edu>
2018-03-20 12:45:27 -0500

    Renamed to image type A and B

--- Utils/Generalized_CycleGAN.py
+++ Utils/Generalized_CycleGAN.py
@@ -6,17 +6,18 @@ Created on Wed May 31 14:06:34 2017
 """
 import sys
 import os
-# Must have Utils.py on path
-sys.path.insert(1,'/home/jmj136/deep-learning/Utils')
 from matplotlib import pyplot as plt
 import numpy as np
 import h5py
 import time
+# progress bar
 from tqdm import tqdm, trange
-# these are my .py scripts
+# these are my .py scripts in Utils
 import Models
-import imageio
 from VisTools import multi_slice_viewer0
+# for gifs
+import imageio
+# for comparing images
 from skimage.measure import compare_ssim as ssim
 
 # Use first available GPU
@@ -32,8 +33,11 @@ np.random.seed(seed=1)
 # Parameters/variables to set
 #~#~#~#~#~#~#~#~#~#~#~#~#~#~#~#
 
+# A is the input image type
+# B is the target image type
+
 # Model Save Path/name
-model_filepath = 'DeepMuMapCyleGAN_{}_model.h5'
+model_filepath = 'CyleGAN_model.h5'
 
 # Data path/name
 datapath = 'CycleGAN_data_TVT.hdf5'
@@ -66,8 +70,8 @@ noStride = 2
 use_bn = False
 # True for regression output
 # False for discrete output
-CT_reg = False
-MR_reg = True
+A_reg = True
+B_reg = False
 
 # whether to plot progress images
 plot_prog = True
@@ -95,13 +99,13 @@ train_rat = 3
 if not 'x_train' in locals():
     print('Loading data...')
     with h5py.File(datapath,'r') as f:
-        test_MR = np.array(f.get('MR_test'))
-        test_CT = np.array(f.get('CT_test_dis'))
+        test_A = np.array(f.get('A_test'))
+        test_B = np.array(f.get('B_test'))
         if training:
-            train_MR = np.array(f.get('MR_train'))
-            train_CT = np.array(f.get('CT_train_dis'))
-            val_MR = np.array(f.get('MR_val'))
-            val_CT = np.array(f.get('CT_val_dis'))
+            train_A = np.array(f.get('A_train'))
+            train_B = np.array(f.get('B_train'))
+            val_A = np.array(f.get('A_val'))
+            val_B = np.array(f.get('B_val'))
         
 #%% Setup models for training
 print("Generating models...")
@@ -109,118 +113,118 @@ from keras.optimizers import Adam
 import keras.backend as K
 
 # Discriminator models
-# CT is target
-# MR is input
-# Cycle loss is between MR to recovered MR
-DisModel_CT = Models.CycleGANdiscriminator(train_CT.shape[1:],numFilters_D,numBlocks_D)
-DisModel_MR = Models.CycleGANdiscriminator(train_MR.shape[1:],numFilters_D,numBlocks_D)
+# A is input
+# B is target
+# Cycle loss is between input A to recovered A
+DisModel_A = Models.CycleGANdiscriminator(train_A.shape[1:],numFilters_D,numBlocks_D)
+DisModel_B = Models.CycleGANdiscriminator(train_B.shape[1:],numFilters_D,numBlocks_D)
 
 # Generator Models
-GenModel_MR2CT = Models.CycleGANgenerator(train_MR.shape[1:],train_CT.shape[-1],
-                                          numFilters_G,numBlocks_G,noStride,use_bn,CT_reg)
-GenModel_CT2MR = Models.CycleGANgenerator(train_CT.shape[1:],train_MR.shape[-1],
-                                          numFilters_G,numBlocks_G,noStride,use_bn,MR_reg)
+GenModel_A2B = Models.CycleGANgenerator(train_A.shape[1:],train_B.shape[-1],
+                                          numFilters_G,numBlocks_G,noStride,use_bn,B_reg)
+GenModel_B2A = Models.CycleGANgenerator(train_B.shape[1:],train_A.shape[-1],
+                                          numFilters_G,numBlocks_G,noStride,use_bn,A_reg)
 
-# Endpoints of graph- MR to CT
-real_MR = GenModel_MR2CT.inputs[0]
-fake_CT = GenModel_MR2CT.outputs[0]
-rec_MR = GenModel_CT2MR([fake_CT])
-# Endpoints of graph- CT to MR
-real_CT = GenModel_CT2MR.inputs[0]
-fake_MR = GenModel_CT2MR.outputs[0]
-rec_CT = GenModel_MR2CT([fake_MR])
-# MR to CT and back generator function
-fn_genCT = K.function([real_MR],[fake_CT,rec_MR])
+# Endpoints of graph- A to B
+real_A = GenModel_A2B.inputs[0]
+fake_B = GenModel_A2B.outputs[0]
+rec_A = GenModel_B2A([fake_B])
+# Endpoints of graph- B to A
+real_B = GenModel_B2A.inputs[0]
+fake_A = GenModel_B2A.outputs[0]
+rec_B = GenModel_A2B([fake_A])
+# A to B and back generator function
+fn_genB = K.function([real_A],[fake_B,rec_A])
 
 # Discriminator scores
-realCTscore = DisModel_CT([real_CT])
-fakeCTscore = DisModel_CT([fake_CT])
-realMRscore = DisModel_MR([real_MR])
-fakeMRscore = DisModel_MR([fake_MR])
+realBscore = DisModel_B([real_B])
+fakeBscore = DisModel_B([fake_B])
+realAscore = DisModel_A([real_A])
+fakeAscore = DisModel_A([fake_A])
 
-#%% CT discriminator loss function
+#%% B discriminator loss funBion
 # create mixed output for gradient penalty
 from keras.layers import Input
 ep_input1 = K.placeholder(shape=(None,1,1,1))
-mixed_CT = Input(shape=train_CT.shape[1:],
-                    tensor=ep_input1 * real_CT + (1-ep_input1) * fake_CT)
-mixedCTscore = DisModel_CT([mixed_CT])
+mixed_B = Input(shape=train_B.shape[1:],
+                    tensor=ep_input1 * real_B + (1-ep_input1) * fake_B)
+mixedBscore = DisModel_B([mixed_B])
 # discriminator losses
-realCTloss = K.mean(realCTscore)
-fakeCTloss = K.mean(fakeCTscore)
+realBloss = K.mean(realBscore)
+fakeBloss = K.mean(fakeBscore)
 # gradient penalty loss
-grad_mixed = K.gradients([mixedCTscore],[mixed_CT])[0]
+grad_mixed = K.gradients([mixedBscore],[mixed_B])[0]
 norm_grad_mixed = K.sqrt(K.sum(K.square(grad_mixed), axis=[1,2,3]))
 grad_penalty = K.mean(K.square(norm_grad_mixed-1))
 # composite Discriminator loss
-loss_CT = fakeCTloss - realCTloss + λ * grad_penalty
+loss_B = fakeBloss - realBloss + λ * grad_penalty
 
-#%% MR to CT generator loss function
-# Adversarial Loss + MR-CT-MR cycle loss
-loss_MR2CT = -fakeCTloss
-loss_MR2CT2MR = K.mean(K.abs(rec_MR-real_MR))
+#%% A to B generator loss function
+# Adversarial Loss + A-B-A cycle loss
+loss_A2B = -fakeBloss
+loss_A2B2A = K.mean(K.abs(rec_A-real_A))
 
-#%% MR discriminator loss function
+#%% A discriminator loss function
 # create mixed output for gradient penalty
 ep_input2 = K.placeholder(shape=(None,1,1,1))
-mixed_MR = Input(shape=train_MR.shape[1:],
-                    tensor=ep_input2 * real_MR + (1-ep_input2) * fake_MR)
-mixedMRscore = DisModel_MR([mixed_MR])
+mixed_A = Input(shape=train_A.shape[1:],
+                    tensor=ep_input2 * real_A + (1-ep_input2) * fake_A)
+mixedAscore = DisModel_A([mixed_A])
 # discriminator losses
-realMRloss = K.mean(realMRscore)
-fakeMRloss = K.mean(fakeMRscore)
+realAloss = K.mean(realAscore)
+fakeAloss = K.mean(fakeAscore)
 # gradient penalty loss
-grad_mixed = K.gradients([mixedMRscore],[mixed_MR])[0]
+grad_mixed = K.gradients([mixedAscore],[mixed_A])[0]
 norm_grad_mixed = K.sqrt(K.sum(K.square(grad_mixed), axis=[1,2,3]))
 grad_penalty = K.mean(K.square(norm_grad_mixed-1))
 # composite Discriminator loss
-loss_MR = fakeMRloss - realMRloss + λ * grad_penalty
+loss_A = fakeAloss - realAloss + λ * grad_penalty
 
-#%% CT to MR generator loss function
-# Adversarial Loss + MR-CT-MR cycle loss
-loss_CT2MR = -fakeMRloss
-loss_CT2MR2CT = K.mean(K.abs(rec_CT-real_CT))
+#%% B to A generator loss function
+# Adversarial Loss + A-B-A cycle loss
+loss_B2A = -fakeAloss
+loss_B2A2B = K.mean(K.abs(rec_B-real_B))
 
 #%% Training functions
 # Discriminator training function
-loss_D = loss_CT + loss_MR
-weights_D = DisModel_CT.trainable_weights + DisModel_MR.trainable_weights
+loss_D = loss_B + loss_A
+weights_D = DisModel_B.trainable_weights + DisModel_A.trainable_weights
 D_trups = Adam(lr=lrD, beta_1=0.0, beta_2=0.9).get_updates(weights_D,[],loss_D)
-fn_trainD = K.function([real_MR, real_CT, ep_input1, ep_input2],[loss_CT, loss_MR], D_trups)
+fn_trainD = K.function([real_A, real_B, ep_input1, ep_input2],[loss_B, loss_A], D_trups)
 
-# Generator Training function
-loss_G = loss_MR2CT + C*loss_MR2CT2MR + loss_CT2MR + C*loss_CT2MR2CT
-weights_G = GenModel_MR2CT.trainable_weights + GenModel_CT2MR.trainable_weights
-MR2CT_trups = Adam(lr=lrG, beta_1=0.0, beta_2=0.9).get_updates(weights_G,[], loss_G)
-# Generator training function returns MR2CT discriminator loss and MR2CT2MR cycle loss
-fn_trainG = K.function([real_MR,real_CT], [loss_MR2CT,loss_MR2CT2MR], MR2CT_trups)
+# Generator Training funBion
+loss_G = loss_A2B + C*loss_A2B2A + loss_B2A + C*loss_B2A2B
+weights_G = GenModel_A2B.trainable_weights + GenModel_B2A.trainable_weights
+A2B_trups = Adam(lr=lrG, beta_1=0.0, beta_2=0.9).get_updates(weights_G,[], loss_G)
+# Generator training funBion returns A2B discriminator loss and A2B2A cycle loss
+fn_trainG = K.function([real_A,real_B], [loss_A2B,loss_A2B2A], A2B_trups)
 
-# validation evaluate function
-fn_evalCycle = K.function([real_MR],[loss_MR2CT2MR])
+# validation evaluate funBion
+fn_evalCycle = K.function([real_A],[loss_A2B2A])
 
 #%% Progress plotting function
-def ProgressPlot(test_MR,test_CT,ex_ind,MR_reg,CT_reg,ax):
-    MR_samp = test_MR[ex_ind,...][np.newaxis,...]
-    [CTtest,rec] = fn_genCT([MR_samp])
-    if CT_reg:
-        CTinds = np.argmax(CTtest[0],axis=-1)
-        CT_current = CTinds/CTtest.shape[-1]
-        CTinds_truth = np.argmax(test_CT[ex_ind],axis=-1)
-        CT_truth = CTinds_truth/CTtest.shape[-1]
+def ProgressPlot(test_A,test_B,ex_ind,A_reg,B_reg,ax):
+    A_samp = test_A[ex_ind,...][np.newaxis,...]
+    [Btest,rec] = fn_genB([A_samp])
+    if B_reg:
+        Binds = np.argmax(Btest[0],axis=-1)
+        B_current = Binds/Btest.shape[-1]
+        Binds_truth = np.argmax(test_B[ex_ind],axis=-1)
+        B_truth = Binds_truth/Btest.shape[-1]
     else:
-        CT_current = CTtest[0,...,0]
-        CT_truth = test_CT[ex_ind,...,0]
-    if MR_reg:
-        MRinds = np.argmax(MR_samp[0],axis=-1)
-        MR_current = MRinds/test_MR.shape[-1]
-        MRinds_rec = np.argmax(rec[0],axis=-1)
-        MR_rec = MRinds_rec/rec.shape[-1]
+        B_current = Btest[0,...,0]
+        B_truth = test_B[ex_ind,...,0]
+    if A_reg:
+        Ainds = np.argmax(A_samp[0],axis=-1)
+        A_current = Ainds/test_A.shape[-1]
+        Ainds_rec = np.argmax(rec[0],axis=-1)
+        A_rec = Ainds_rec/rec.shape[-1]
     else:
-        MR_current = MR_samp[0,...,0]
-        MR_rec= rec[0,...,0]    
+        A_current = A_samp[0,...,0]
+        A_rec= rec[0,...,0]    
         
-    samp_im1 = np.c_[MR_current,CT_current]
-    samp_im2 = np.c_[MR_rec,CT_truth]
+    samp_im1 = np.c_[A_current,B_current]
+    samp_im2 = np.c_[A_rec,B_truth]
     samp_im = np.r_[samp_im1,samp_im2]
     ax.imshow(samp_im,cmap='gray',vmin=0,vmax=1)
     ax.set_axis_off()
@@ -237,13 +241,13 @@ print('Starting training...')
 dis_loss = np.zeros((numIter,2))
 gen_loss = np.zeros((numIter,2))
 val_loss = np.ones((np.int(numIter/valstep),2))
-templosses = np.zeros((np.int(val_MR.shape[0]/val_b_s),2))
+templosses = np.zeros((np.int(val_A.shape[0]/val_b_s),2))
 
 # Updatable plot
 if plot_prog:
     plt.ion()
     fig, ax = plt.subplots()
-    _ = ProgressPlot(test_MR,test_CT,ex_ind,MR_reg,CT_reg,ax)
+    _ = ProgressPlot(test_A,test_B,ex_ind,A_reg,B_reg,ax)
     # preallocate image display
     progress_ims = np.zeros((np.int(numIter/progstep),2*256,2*256))
     gg = 0
@@ -259,37 +263,37 @@ for ii in t:
     for _ in range(train_rat):
         # Train Discriminator
         # grab random training samples
-        batch_inds = np.random.choice(train_MR.shape[0], b_s, replace=False)
-        MR_batch = train_MR[batch_inds,...]
-        batch_inds = np.random.choice(train_CT.shape[0], b_s, replace=False)
-        CT_batch = train_CT[batch_inds,...]
+        batch_inds = np.random.choice(train_A.shape[0], b_s, replace=False)
+        A_batch = train_A[batch_inds,...]
+        batch_inds = np.random.choice(train_B.shape[0], b_s, replace=False)
+        B_batch = train_B[batch_inds,...]
         # train discrimator
         ϵ1 = np.random.uniform(size=(b_s, 1, 1 ,1))
         ϵ2 = np.random.uniform(size=(b_s, 1, 1 ,1))
-        errD  = fn_trainD([MR_batch, CT_batch, ϵ1,ϵ2])
+        errD  = fn_trainD([A_batch, B_batch, ϵ1,ϵ2])
     dis_loss[ii] = errD
     # Train Generator
-    errG = fn_trainG([MR_batch, CT_batch])
+    errG = fn_trainG([A_batch, B_batch])
     gen_loss[ii] = errG
     if plot_prog:
         if ii % progstep == 0:
             # progress image plotting
-            samp_im = ProgressPlot(test_MR,test_CT,ex_ind,MR_reg,CT_reg,ax)
+            samp_im = ProgressPlot(test_A,test_B,ex_ind,A_reg,B_reg,ax)
             progress_ims[gg] = samp_im
             gg += 1
     if (ii+1) % valstep ==0:
         tqdm.write('Checking validation loss...')
         for bb in range(0,templosses.shape[0]):
-            val_inds = np.arange(bb*val_b_s,np.minimum((bb+1)*val_b_s,val_MR.shape[0]))
-            MR_batch = val_MR[val_inds,...]
-            valL1 = fn_evalCycle([MR_batch])[0]
+            val_inds = np.arange(bb*val_b_s,np.minimum((bb+1)*val_b_s,val_A.shape[0]))
+            A_batch = val_A[val_inds,...]
+            valL1 = fn_evalCycle([A_batch])[0]
             templosses[bb] = valL1
         
         cur_val_loss = np.mean(templosses,axis=0)
         if cur_val_loss[0] <= np.min(val_loss,axis=0)[0]:
             tqdm.write('Valdiation loss decreased to {:.02e}'.format(cur_val_loss[0]))
-            GenModel_MR2CT.save(model_filepath.format('MR2CT'),True,False)
-            GenModel_CT2MR.save(model_filepath.format('CT2MR'),True,False)
+            GenModel_A2B.save(model_filepath.format('A2B'),True,False)
+            GenModel_B2A.save(model_filepath.format('B2A'),True,False)
             tqdm.write("Saved models to file")
         else:
             tqdm.write('Validation loss did not decrease: {:.02e}'.format(cur_val_loss[0]))
@@ -306,15 +310,15 @@ print('Training complete')
 
 # backup model saving- writes as weights and network structure
 # which is more reliable than saving model as single file
-model_json = GenModel_MR2CT.to_json()
-with open("BackupModel_MR2CT.json", "w") as json_file:
+model_json = GenModel_A2B.to_json()
+with open("BackupModel_A2B.json", "w") as json_file:
     json_file.write(model_json)
-GenModel_MR2CT.save_weights("BackupModel_MR2CT.h5")
+GenModel_A2B.save_weights("BackupModel_A2B.h5")
 
-model_json = GenModel_CT2MR.to_json()
-with open("BackupModel_CT2MR.json", "w") as json_file:
+model_json = GenModel_B2A.to_json()
+with open("BackupModel_B2A.json", "w") as json_file:
     json_file.write(model_json)
-GenModel_CT2MR.save_weights("BackupModel_CT2MR.h5")
+GenModel_B2A.save_weights("BackupModel_B2A.h5")
 
 print('Backup Models saved')
 
@@ -327,8 +331,8 @@ plt.plot(np.arange(numIter),-medfilt(dis_loss[:,0],5),
          np.arange(numIter),-medfilt(dis_loss[:,1],5),
          np.arange(numIter),medfilt(1000*gen_loss[:,1],5),
          np.arange(0,numIter,valstep),1000*val_loss[:,0])
-plt.legend(['-Discriminator CT Loss',
-            '-Discriminator MR Loss',
+plt.legend(['-Discriminator B Loss',
+            '-Discriminator A Loss',
             '1000x Cycle Loss',
             '1000x Validation Cycle loss'])
 plt.ylim([0,60])
@@ -341,28 +345,28 @@ from keras.models import load_model
 if False:
     # Load backup models
     from keras.models import model_from_json
-    json_file = open('BackupModel_MR2CT.json', 'r')
+    json_file = open('BackupModel_A2B.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
-    GenModel_MR2CT = model_from_json(loaded_model_json)
-    GenModel_MR2CT.load_weights("BackupModel_MR2CT.h5")
+    GenModel_A2B = model_from_json(loaded_model_json)
+    GenModel_A2B.load_weights("BackupModel_A2B.h5")
     from keras.models import model_from_json
-    json_file = open('BackupModel_CT2MR.json', 'r')
+    json_file = open('BackupModel_B2A.json', 'r')
     loaded_model_json = json_file.read()
     json_file.close()
-    GenModel_CT2MR = model_from_json(loaded_model_json)
-    GenModel_CT2MR.load_weights("BackupModel_CT2MR.h5")
+    GenModel_B2A = model_from_json(loaded_model_json)
+    GenModel_B2A.load_weights("BackupModel_B2A.h5")
     print("Loaded backup models from weights")
 else:
     # Load regular checkpointed models
-    GenModel_MR2CT = load_model(model_filepath.format('MR2CT'),None,False)
-    GenModel_CT2MR = load_model(model_filepath.format('CT2MR'),None,False)
+    GenModel_A2B = load_model(model_filepath.format('A2B'),None,False)
+    GenModel_B2A = load_model(model_filepath.format('B2A'),None,False)
 
 # get generator results
 time1 = time.time()
-CTtest = GenModel_MR2CT.predict(test_MR)
+Btest = GenModel_A2B.predict(test_A)
 time2 = time.time()
-print('Infererence time: ',1000*(time2-time1)/test_MR.shape[0],' ms per slice')
+print('Infererence time: ',1000*(time2-time1)/test_A.shape[0],' ms per slice')
 
 # Display progress images, if they exist
 
@@ -378,49 +382,41 @@ if 'progress_ims' in locals():
     images = [gif_ims[ii,...] for ii in range(gif_ims.shape[0])]
     imageio.mimsave(output_file, images, duration=1/6,loop=1)
 
-if CT_reg:
+if B_reg:
     # Calculate SSIM between test images
-    SSIMs = [ssim(im1,im2) for im1, im2 in zip(test_CT[...,0],CTtest[...,0])]
+    SSIMs = [ssim(im1,im2) for im1, im2 in zip(test_B[...,0],Btest[...,0])]
     print('Mean SSIM of ', np.mean(SSIMs))
     print('SSIM range of ', np.round(np.min(SSIMs),3), ' - ', np.round(np.max(SSIMs),3))
 
 # Display test images in grid
-# Input MR     | Output CT
+# Input A     | Output B
 #----------------------
-# Recovered MR | Actual CT
+# Recovered A | Actual B
 
-MRrec = GenModel_CT2MR.predict(CTtest)
+Arec = GenModel_B2A.predict(Btest)
 
-if CT_reg:
-    CTinds = np.argmax(CTtest,axis=-1)
-    CT_output = CTinds/CTtest.shape[-1]
-    CTinds_truth = np.argmax(test_CT,axis=-1)
-    CT_truth = CTinds_truth/CTtest.shape[-1]
+if B_reg:
+    Binds = np.argmax(Btest,axis=-1)
+    B_output = Binds/Btest.shape[-1]
+    Binds_truth = np.argmax(test_B,axis=-1)
+    B_truth = Binds_truth/Btest.shape[-1]
 else:
-    CT_output = CTtest[...,0]
-    CT_truth = test_CT[...,0]
-if MR_reg:
-    MRinds = np.argmax(test_MR,axis=-1)
-    MR_output = MRinds/test_MR.shape[-1]
-    MRinds_rec = np.argmax(MRrec,axis=-1)
-    MR_rec = MRinds_rec/MRrec.shape[-1]
+    B_output = Btest[...,0]
+    B_truth = test_B[...,0]
+if A_reg:
+    Ainds = np.argmax(test_A,axis=-1)
+    A_output = Ainds/test_A.shape[-1]
+    Ainds_rec = np.argmax(Arec,axis=-1)
+    A_rec = Ainds_rec/Arec.shape[-1]
 else:
-    MR_output = test_MR[...,0]
-    MR_rec= MRrec[...,0]
+    A_output = test_A[...,0]
+    A_rec= Arec[...,0]
         
-disp_im1 = np.c_[MR_output,CT_output]
-disp_im2 = np.c_[MR_rec,CT_truth]
+disp_im1 = np.c_[A_output,B_output]
+disp_im2 = np.c_[A_rec,B_truth]
 test_disp = np.concatenate((disp_im1,disp_im2),axis=1)
 
-if CT_reg:
+if B_reg:
     multi_slice_viewer0(test_disp,'Test Images',SSIMs)
 else:
     multi_slice_viewer0(test_disp,'Test Images')
-
-# Load full MR data and run inferencing for visual inspection
-#with h5py.File('CycleGAN_FullMRdata.hdf5','r') as f:
-#        full_MR = np.array(f.get('MR_full'))
-#full_CT = GenModel_MR2CT.predict(full_MR)
-#full_MRrec = GenModel_CT2MR.predict(full_CT)
-#full_disp = np.concatenate((full_MR[...,0],full_CT[...,0],full_MRrec[...,0]),axis=2)
-#multi_slice_viewer0(full_disp,'Full Images')
