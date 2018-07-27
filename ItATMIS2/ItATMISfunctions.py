762d9aa2992c16701ee6c5511e9887657a73c00e
Johnson Jacob M <johnsonj118@gmail.com>
2018-07-27 11:18:52 -0500

    work in progress. made copies for breast testing task

--- ItATMIS2/ItATMISfunctions.py
+++ ItATMIS2/ItATMISfunctions.py
@@ -98,3 +98,11 @@ def BlockModel(in_shape,filt_num=16,numBlocks=4,num_out_channels=2):
     lay_out = Conv2D(num_out_channels,(1,1), activation='sigmoid',name='output_layer')(lay_pad)
     
     return Model(lay_input,lay_out)
+
+#%% Dice Coefficient Loss
+import keras.backend as K
+def dice_coef_loss(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return 1-(2. * intersection + 1) / (K.sum(y_true_f) + K.sum(y_pred_f) + 1)
