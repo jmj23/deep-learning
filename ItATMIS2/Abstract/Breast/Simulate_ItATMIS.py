e9b70f38eaddfadcd7688fc04c971e1188b35d10
Johnson Jacob M <johnsonj118@gmail.com>
2018-07-30 11:53:54 -0500

    Added CV loop for testing

--- ItATMIS2/Abstract/Breast/Simulate_ItATMIS.py
+++ ItATMIS2/Abstract/Breast/Simulate_ItATMIS.py
@@ -29,12 +29,16 @@ except RuntimeError as e:
     print('No GPU available')
     print('Using CPU')
     os.environ["CUDA_VISIBLE_DEVICES"] = ""
+    
+import warnings
+warnings.filterwarnings("ignore")
 #%% Parameters
 data_dir = '/data/jmj136/ItATMIS/Breast'
 model_weights_path = '/home/jmj136/deep-learning/ItATMIS2/Abstract/Breast/best_model_weights.h5'
 val_frac = 0.2
 cross_val_num = 1
-maxEpochs = 5
+num_CV_folds = 3
+maxIters = 5
 
 cb_check = ModelCheckpoint(model_weights_path,
                            monitor='val_loss',
@@ -56,47 +60,47 @@ for subj in subjs:
     targets.append(mat['TargetArray'][...,0])
 
 #%% Iterate for each cross-validation
-it = 0
-# make model
-model = BlockModel(inputs[0].shape,filt_num=16,numBlocks=4,num_out_channels=1)
-model.compile(optimizer=Adam(), loss=dice_coef_loss)
-
-# split off cross-validation subjects
-cv_subjs = random.sample(subjs,cross_val_num)
-cv_inputs = np.concatenate([i for j, i in enumerate(inputs) if j in cv_subjs])
-cv_targets = np.concatenate([i for j, i in enumerate(targets) if j in cv_subjs])[...,np.newaxis]
-train_inputs = [i for j, i in enumerate(inputs) if j not in cv_subjs]
-train_targets = [i for j, i in enumerate(targets) if j not in cv_subjs]
-
-combined = list(zip(train_inputs, train_targets))
-random.shuffle(combined)
-train_inputs[:], train_targets[:] = zip(*combined)
-it += 1
-#%% Iteratively train and add subjects
-
-# list for collecting losses
-CV_losses = []
-# maximim iterations
-maxIter = np.minimum(len(train_inputs),maxEpochs)
-for cur_num_subj in range(1,maxIter+1):
-    cur_inputs = np.concatenate(train_inputs[:cur_num_subj])
-    cur_targets = np.concatenate(train_targets[:cur_num_subj])[...,np.newaxis]
-    print('Including {} subjects on CV fold {}'.format(cur_num_subj,it))
-    time1 = time()
-    model = SimulateItATMIS(model,cur_inputs,cur_targets,CBs,val_frac)
-    time2 = time()
-    print('Training complete')
-    print('Time elapsed: {:02d} s'.format(time2-time1))
-    print('Evaluating model on cross-validation data')
-    model.load_weights(model_weights_path)
-    score = model.evaluate(cv_inputs,cv_targets)
-    print("Cross-validation Dice score is {:.03f}".format(1-score))
-    CV_losses.append(1-score)
-
-# save results
-print('Cross validation fold complete. Saving results...')
-results_path = '/home/jmj136/deep-learning/ItATMIS2/Abstract/Results/ItATMIS_SimResults_{}_CV{}'.format('breast',''.join(map(str,cv_subjs)))
-np.savetxt(results_path,np.array(CV_losses))
+    
+for it in range(maxIters):
+    # make model
+    model = BlockModel(inputs[0].shape,filt_num=16,numBlocks=4,num_out_channels=1)
+    model.compile(optimizer=Adam(), loss=dice_coef_loss)
+    
+    # split off cross-validation subjects
+    cv_subjs = random.sample(subjs,cross_val_num)
+    cv_inputs = np.concatenate([i for j, i in enumerate(inputs) if j in cv_subjs])
+    cv_targets = np.concatenate([i for j, i in enumerate(targets) if j in cv_subjs])[...,np.newaxis]
+    train_inputs = [i for j, i in enumerate(inputs) if j not in cv_subjs]
+    train_targets = [i for j, i in enumerate(targets) if j not in cv_subjs]
+    
+    combined = list(zip(train_inputs, train_targets))
+    random.shuffle(combined)
+    train_inputs[:], train_targets[:] = zip(*combined)
+    #%% Iteratively train and add subjects
+    
+    # list for collecting losses
+    CV_losses = []
+    # maximim iterations
+    maxIter = np.minimum(len(train_inputs),maxIters)
+    for cur_num_subj in range(1,maxIter+1):
+        cur_inputs = np.concatenate(train_inputs[:cur_num_subj])
+        cur_targets = np.concatenate(train_targets[:cur_num_subj])[...,np.newaxis]
+        print('Including {} subjects on CV fold {}'.format(cur_num_subj,it+1))
+        time1 = time()
+        model = SimulateItATMIS(model,cur_inputs,cur_targets,CBs,val_frac)
+        time2 = time()
+        print('Training complete')
+        print('Time elapsed: {:0.2f} s'.format(time2-time1))
+        print('Evaluating model on cross-validation data')
+        model.load_weights(model_weights_path)
+        score = model.evaluate(cv_inputs,cv_targets,verbose=0)
+        print("Cross-validation Dice score is {:.03f}".format(1-score))
+        CV_losses.append(1-score)
+    
+    # save results
+    print('Cross validation fold complete. Saving results...')
+    results_path = '/home/jmj136/deep-learning/ItATMIS2/Abstract/Results/ItATMIS_SimResults_{}_CV{}.txt'.format('breast',''.join(map(str,cv_subjs)))
+    np.savetxt(results_path,np.array(CV_losses))
 
 # plot score results
 print('All cross-validation folds complete!')
