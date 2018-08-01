382bc7a74560c5bef1d9fb2d4d8b83124f1c01f4
Jacob Johnson <jmjohnson33@wisc.edu>
2018-08-01 12:08:01 -0500

    Added lung data getter

--- ItATMIS2/ItATMISfunctions.py
+++ ItATMIS2/ItATMISfunctions.py
@@ -209,3 +209,71 @@ def PlotErrorResults(anatomy):
     plt.ylabel('Dice')
     plt.legend()
     plt.ylim([0,1])
+    
+#%% LCTSC data getter
+import os
+from skimage.draw import polygon
+import pydicom
+from skimage.transform import resize
+
+def GetLCTSCdata(directory):
+    cur_dir = glob.glob(os.path.join(directory, "*", ""))[0]
+    dcm_dir = glob.glob(os.path.join(cur_dir, "0*", ""))[0]
+    lbl_dir = glob.glob(os.path.join(cur_dir, "1*", ""))[0]
+    dicom_files = glob.glob(os.path.join(dcm_dir, "*.dcm"))
+    lbl_file = glob.glob(os.path.join(lbl_dir,"*.dcm"))[0]
+    dicms = [pydicom.read_file(fn) for fn in dicom_files]
+    dicms.sort(key = lambda x: float(x.ImagePositionPatient[2]))
+    ims = np.stack([dcm.pixel_array.astype(np.float) for dcm in dicms])
+    # normalize
+    for im in ims:
+        im -= np.mean(im)
+        im /= np.std(im)
+    # resize
+    # pre-allocate array
+    inputs = np.zeros((ims.shape[0],256,256))
+    # iterate over all the input images and resize
+    for i,im in enumerate(ims):
+        inputs[i] = resize(ims,(256,256))
+        
+    # get labels
+    label = pydicom.read_file(lbl_file)
+    contour_names = [s.ROIName for s in label.StructureSetROISequence]
+    # Get the right and left lung indices
+    r_ind = contour_names.index('Lung_R')
+    l_ind = contour_names.index('Lung_L')
+    # Extract the corresponding contours and combine
+    contour_right = [s.ContourData for s in label.ROIContourSequence[r_ind].ContourSequence]
+    contour_left = [s.ContourData for s in label.ROIContourSequence[l_ind].ContourSequence]
+    contours = contour_left + contour_right
+    # Z positions
+    z = [d.ImagePositionPatient[2] for d in dicms]
+    # Rows and columns
+    pos_r = dicms[0].ImagePositionPatient[1]
+    spacing_r = dicms[0].PixelSpacing[1]
+    pos_c = dicms[0].ImagePositionPatient[0]
+    spacing_c = dicms[0].PixelSpacing[0]
+    # Preallocate
+    mask = np.zeros_like(ims)
+    # loop over the different slices that each contour is on
+    for c in contours:
+        nodes = np.array(c).reshape((-1, 3))
+        assert np.amax(np.abs(np.diff(nodes[:, 2]))) == 0
+        zNew = [round(elem,1) for elem in z]
+        try:
+            z_index = z.index(nodes[0,2])
+        except ValueError:
+            z_index = zNew.index(nodes[0,2])
+        r = (nodes[:, 1] - pos_r) / spacing_r
+        c = (nodes[:, 0] - pos_c) / spacing_c
+        rr, cc = polygon(r, c)
+        mask[z_index,rr, cc] = 1
+        
+    # resize
+    # pre-allocate array
+    targets = np.zeros((mask.shape[0],256,256))
+    # iterate over all the target images and resize
+    for i,im in enumerate(mask):
+        targets[i] = resize(mask,(256,256))   
+        
+    return inputs,targets
