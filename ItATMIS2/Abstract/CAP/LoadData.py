974fb50f25dfed345ffc4afc3795ebfa7c6d6c2b
Jacob Johnson <jmjohnson33@wisc.edu>
2018-08-02 11:57:17 -0500

    Do-over at trying to load CAP data

--- ItATMIS2/Abstract/CAP/LoadData.py
+++ ItATMIS2/Abstract/CAP/LoadData.py
@@ -1,76 +1,83 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Thu Jul 26 13:57:31 2018
+Created on Wed Aug  1 12:15:45 2018
 
 @author: jmj136
 """
+#%%
+import glob
+import pydicom as dcm
 import os
 import numpy as np
-import h5py
-import pydicom as dcm
-from glob import glob
-from skimage.draw import polygon
-from operator import itemgetter
-#%% directories
-data_dir = '/data/jmj136/ItATMIS/CAP/SCD_DeidentifiedImages/'
-annote_dir = '/data/jmj136/ItATMIS/CAP/SCD_ManualContours/'
-# SC-HF-I-01/contours-manual/IRCCI-expert
-savepath = '/data/jmj136/ItATMIS/CAP/data.hdf5'
+from natsort import natsorted
+from matplotlib import pyplot as plt
+import sys
+sys.path.insert(1,'/home/jmj136/deep-learning/Utils')
+from VisTools import slice_viewer4D
+data_dir = '/home/jmj136/Data/ItATMIS/CAP'
+#%%
+def GetDcmVolume(directory):
+    files = natsorted(glob.glob(os.path.join(directory,"*.dcm")))
+    dicoms = [dcm.dcmread(f) for f in files]
+    images = np.stack([d.pixel_array for d in dicoms])
+    return images    
+#%%
+cur_dirs = glob.glob(os.path.join(data_dir, "*", ""))
+image_dir = cur_dirs[0]
+contour_dir = cur_dirs[1]
+subj_dirs = natsorted(glob.glob(os.path.join(image_dir, "*", "")))
 
-# find subject directories
-subj_dirs = glob(os.path.join(data_dir, "*", ""))
+cur_subj_dir = subj_dirs[0]
 
-# loop over subjects
-subj = 0
-# current subject directory
-cur_dir = subj_dirs[0]
-# get list of dicom files
+cur_dirs = natsorted(glob.glob(os.path.join(cur_subj_dir, "*-z_+x_256*", "")))
+
+images = np.stack([GetDcmVolume(d) for d in cur_dirs[:7]])
+slice_viewer4D(images/1000)
+
+#%%
+subj_dirs = natsorted(glob.glob(os.path.join(contour_dir, "*", "")))
+cur_subj_dir = subj_dirs[0]
+
+cur_dir = natsorted(glob.glob(os.path.join(cur_subj_dir, "*", "")))[0]
+
+cur_dir = natsorted(glob.glob(os.path.join(cur_dir, "*", "")))[0]
+
+files = natsorted(glob.glob(os.path.join(cur_dir, "*")))
+
+test = np.loadtxt(files[0])
+pos_r = 0
+pos_c = 0
+spacing_r = 1.367188
+spacing_c = 1.367188
+r = (test[:, 1] - pos_r) / spacing_r
+c = (test[:, 0] - pos_c) / spacing_c
+plt.plot(c,r,'-')
+
+for f in files[1:3]:
+    test = np.loadtxt(f)
+    plt.plot(test[:,0],test[:,1],'-')
+    
+#%%
 dcm_files = []
-for root, dirs, files in os.walk(cur_dir):
+for root, dirs, files in os.walk(cur_subj_dir):
     dcm_files = dcm_files + [(os.path.join(root,f)) for f in files if f.endswith('.dcm')]
+    
+dcms = [dcm.dcmread(f) for f in dcm_files]
 
-# load dicom files
-dcms = [dcm.read_file(f) for f in dcm_files]
-# prepare sorting list of tuples
 dcm_inds = list(range(len(dcms)))
 locs = [(float(d.SliceLocation)) for d in dcms]
 times = [(int(d.InstanceNumber)) for d in dcms]
-loctimes = list(zip(locs,times,dcm_inds))
-# sort by time then slice
-loctimes.sort(key=itemgetter(0))
+rows = [(int(d.Rows)) for d in dcms]
+spacing = [d.PixelSpacing for d in dcms]
+position = [d.ImagePositionPatient for d in dcms]
+loctimes = list(zip(locs,times,rows,dcm_inds))
+from operator import itemgetter
 loctimes.sort(key=itemgetter(1))
-# extract sorting indices
-sort_inds = [ele[2] for ele in loctimes]
+loctimes.sort(key=itemgetter(0))
+loctimes.sort(key=itemgetter(2))
+
+sort_inds = [ele[3] for ele in loctimes]
 # combine into array with sorting order
-image_volume = np.stack([dcms[i].pixel_array for i in sort_inds])
-
-
-
- # Z positions
-z = [d.ImagePositionPatient[2] for d in dcms]
-# Rows and columns
-pos_r = dcms[0].ImagePositionPatient[1]
-spacing_r = dcms[0].PixelSpacing[1]
-pos_c = dcms[0].ImagePositionPatient[0]
-spacing_c = dcms[0].PixelSpacing[0]
-# Preallocate
-mask = np.zeros_like(ims)
-# loop over the different slices that each contour is on
-nodes = np.array(c).reshape((-1, 3))
-assert np.amax(np.abs(np.diff(nodes[:, 2]))) == 0
-zNew = [round(elem,1) for elem in z]
-try:
-    z_index = z.index(nodes[0,2])
-except ValueError:
-    z_index = zNew.index(nodes[0,2])
-r = (nodes[:, 1] - pos_r) / spacing_r
-c = (nodes[:, 0] - pos_c) / spacing_c
-rr, cc = polygon(r, c)
-mask[z_index,rr, cc] = 1
-    
-with h5py.File(savepath, 'w') as hf:
-    dataname = 'subj_{:03d}_mask'.format(subj)
-    hf.create_dataset(dataname,  data=masks, dtype='f')
-    dataname = 'subj_{:03d}_image'.format(subj)
-    hf.create_dataset(dataname,  data=images, dtype='f')
+image_volume = np.stack([dcms[i].pixel_array for i in sort_inds[165:]])
+multi_slice_viewer0(image_volume/1000)
