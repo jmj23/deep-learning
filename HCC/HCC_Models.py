7531b7b748ad68a3db1b2c1e547dc8c702e6eaa6
Jacob Johnson <johnsonj118@gmail.com>
2019-07-22 10:05:14 -0500

    Added block model classifier

--- HCC/HCC_Models.py
+++ HCC/HCC_Models.py
@@ -2,12 +2,14 @@
 # pylint: disable=bad-whitespace
 # pylint: disable=trailing-whitespace
 import keras.backend as K
-from keras.initializers import glorot_uniform
-from keras.layers import (Activation, Add, AveragePooling2D,
-                          BatchNormalization, Conv2D, Dense, Flatten, Input,
-                          MaxPooling2D, ZeroPadding2D)
-from keras.models import Model
+import numpy as np
 from keras.applications.inception_v3 import InceptionV3
+from keras.initializers import glorot_uniform
+from keras.layers import (ELU, Activation, Add, AveragePooling2D,
+                          BatchNormalization, Conv2D, Dense, Flatten,
+                          GlobalAveragePooling2D, Input, MaxPooling2D,
+                          ZeroPadding2D, concatenate)
+from keras.models import Model
 
 # from resnet_utils import *
 
@@ -194,11 +196,86 @@ def ResNet50(input_shape=(512, 512, 3), classes=1):
     return model
 
 
-def Inception_model(input_shape=(299,299,3)):
-    incep_model = InceptionV3(include_top=False,weights=None,input_shape=input_shape,pooling=None)
+def Inception_model(input_shape=(299, 299, 3)):
+    incep_model = InceptionV3(
+        include_top=False, weights=None, input_shape=input_shape, pooling=None)
     input_layer = incep_model.input
     incep_output = incep_model.output
-    x = Conv2D(16,(3,3),activation='relu')(incep_output)
+    x = Conv2D(16, (3, 3), activation='relu')(incep_output)
     x = Flatten()(x)
-    x = Dense(1,activation='sigmoid')(x)
-    return Model(inputs=input_layer,outputs=x)
+    x = Dense(1, activation='sigmoid')(x)
+    return Model(inputs=input_layer, outputs=x)
+
+
+def BlockModel_Classifier(input_shape, filt_num=16, numBlocks=3):
+    """Creates a Block model for pretraining on classification task
+    Args:
+        input shape: a list or tuple of [rows,cols,channels] of input images
+        filt_num: the number of filters in the first and last layers
+        This number is multipled linearly increased and decreased throughout the model
+        numBlocks: number of processing blocks. The larger the number the deeper the model
+        output_chan: number of output channels. Set if doing multi-class segmentation   
+        regression: Whether to have a continuous output with linear activation
+    Returns:
+        An unintialized Keras model
+
+    Example useage: SegModel = BlockModel2D([256,256,1],filt_num=8)
+
+    Notes: Using rows/cols that are powers of 2 is recommended. Otherwise,
+    the rows/cols must be divisible by 2^numBlocks for skip connections
+    to match up properly
+    """
+
+    # check for input shape compatibility
+    rows, cols = input_shape[0:2]
+    assert rows % 2**numBlocks == 0, "Input rows and number of blocks are incompatible"
+    assert cols % 2**numBlocks == 0, "Input cols and number of blocks are incompatible"
+
+    # calculate size reduction
+    startsize = np.max(input_shape[0:2])
+    minsize = (startsize-np.sum(2**np.arange(1, numBlocks+1)))/2**numBlocks
+    assert minsize > 4, "Too small of input for this many blocks. Use fewer blocks or larger input"
+
+    # input layer
+    lay_input = Input(shape=input_shape, name='input_layer')
+
+    # contracting blocks
+    x = lay_input
+    for rr in range(1, numBlocks+1):
+        x1 = Conv2D(filt_num*rr, (1, 1), padding='same',
+                    name='Conv1_{}'.format(rr))(x)
+        x1 = BatchNormalization()(x1)
+        x1 = ELU(name='elu_x1_{}'.format(rr))(x1)
+        x3 = Conv2D(filt_num*rr, (3, 3), padding='same',
+                    name='Conv3_{}'.format(rr))(x)
+        x3 = BatchNormalization()(x3)
+        x3 = ELU(name='elu_x3_{}'.format(rr))(x3)
+        x51 = Conv2D(filt_num*rr, (3, 3), padding='same',
+                     name='Conv51_{}'.format(rr))(x)
+        x51 = BatchNormalization()(x51)
+        x51 = ELU(name='elu_x51_{}'.format(rr))(x51)
+        x52 = Conv2D(filt_num*rr, (3, 3), padding='same',
+                     name='Conv52_{}'.format(rr))(x51)
+        x52 = BatchNormalization()(x52)
+        x52 = ELU(name='elu_x52_{}'.format(rr))(x52)
+        x = concatenate([x1, x3, x52], name='merge_{}'.format(rr))
+        x = Conv2D(filt_num*rr, (1, 1), padding='valid',
+                   name='ConvAll_{}'.format(rr))(x)
+        x = BatchNormalization()(x)
+        x = ELU(name='elu_all_{}'.format(rr))(x)
+        x = ZeroPadding2D(padding=(1, 1), name='PrePad_{}'.format(rr))(x)
+        x = Conv2D(filt_num*rr, (4, 4), padding='valid',
+                   strides=(2, 2), name='DownSample_{}'.format(rr))(x)
+        x = BatchNormalization()(x)
+        x = ELU(name='elu_downsample_{}'.format(rr))(x)
+        x = Conv2D(filt_num*rr, (3, 3), padding='same',
+                   name='ConvClean_{}'.format(rr))(x)
+        x = BatchNormalization()(x)
+        x = ELU(name='elu_skip_{}'.format(rr))(x)
+
+    # average pooling
+    x = GlobalAveragePooling2D()(x)
+    # classifier
+    lay_out = Dense(1, activation='sigmoid', name='output_layer')(x)
+
+    return Model(lay_input, lay_out)
